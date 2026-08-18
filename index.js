a9faf6c21753280d7ecae955e6ee370df753fbda
Aluno IFAL <alunoifal@MACEIO.IFAL>
2026-08-18 20:23:50 -0300

    fix(rota-01): reponde GET / com "Olá, Mundo!"

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200,
-  res.setHeader('Content-type', 'text/html');
+  res.setHeader('Content-type', 'text');
 
-  res.end('<h1>Olá mundo</h1>')
+  res.end('Olá, Mundo!')
 
 });
 
